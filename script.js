4aaf060081a0aaa409bd4495603c5bdd0df4d976
taegyung <taegyung.e@gmail.com>
2022-10-07 10:59:00 +0900

    작업시작

---
+++ script.js
@@ -0,0 +1,17 @@
+class YutPlay {
+    constructor(target, opt) {
+        this.target = document.querySelector(target);
+        this.canvas = document.createElement('canvas');
+        this.ctx = this.canvas.getContext('2d');
+        this.target.append(this.canvas);
+
+        this.canvas.width = opt.canvasWidth || 500;
+        this.canvas.height = opt.canvasHeight || 400;
+    }
+}
+
+window.onload = () => {
+    new YutPlay('.yutplay', {
+
+    });
+}
